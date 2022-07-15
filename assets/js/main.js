26cf43f7156e806782935b0b8fbcb64f092315a5
Sivagajan <sivagajan@gmail.com>
2022-07-15 11:37:18 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+console.log('it works')
+
+/* let info =  */document.querySelector('#info').innerHTML = '<h1>Hallo Welt</h1>'
+document.querySelector('#info').innerHTML += '<h2>How are you ?</h2>'
+// console.log(info)
+
+let container = document.getElementById('container')
+console.log(container)
+
+container.innerHTML += '<p> start of the Element</p>'
+console.log(container)
+
+
+document.write("end of the element")
